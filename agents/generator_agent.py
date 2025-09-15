f093bca2f0b4619a8e1d8dcfcbc722e489d908b5
Dhiraj Shah <dhirajssh@gmail.com>
2025-09-15 13:57:14 -0700

    feat: completed the generator agent code. generator_agent.py

--- agents/generator_agent.py
+++ agents/generator_agent.py
@@ -1,10 +1,22 @@
+import os
 from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
 from utils import load_system_prompt
+import streamlit as st
 
 model = ChatOllama(model="llama3.1:8b")
+prompt = load_system_prompt("generator.md")
+
+resume_path = os.path.join(os.path.dirname(__file__), "..", "resume.md")
+if os.path.exists(resume_path):
+  resume = load_system_prompt("resume.md")
+  st.session_state.resume = resume
+else:
+  resume = st.session_state.resume
+
+full_prompt = f"{prompt}\n\n---\n\n{resume}"
 
 generator_agent = create_react_agent(
   model=model,
-  prompt=load_system_prompt(".md")
+  prompt=full_prompt
 )
